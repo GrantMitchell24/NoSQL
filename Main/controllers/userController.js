09064808a8e52001f6e9d29313e1c9f07dfbfed4
GrantMitchell24 <GrantMitchell2423@yahoo.com>
2023-11-29 12:41:08 -0600

    updated controllers, need routes next

--- Main/controllers/userController.js
+++ Main/controllers/userController.js
@@ -9,5 +9,60 @@ module.exports = {
     } catch (err) {
       res.status(500).json(err);
     }
-  }
-}
+  },
+
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+
+      if (!user) {
+        return res.status(404).json({ message: 'No course with that ID'});
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createCourse(req, res) {
+    try {
+      const course = await Course.create(req.body);
+      res.json(course);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  async deleteCourse(req, res) {
+    try {
+      const course = await Course.findOneAndDelete({ _id: req.params.courseId});
+
+      if (!course) {
+        res.status(404).json({ message: 'No course with that ID'}); 
+      }
+
+      await Student.deleteMany({ _id: { $in: course.students}});
+      res.json({ message: 'Course and students deleted!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async updateCourse(req, res) {
+    try {
+      const course = await Course.findOneAndUpdate(
+        { _id: req.params.courseId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!course) {
+        res.status(404).json({ message: 'No course with this id!' });
+      }
+
+      res.json(course);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  };
